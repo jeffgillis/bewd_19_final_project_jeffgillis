bbddd281994b5abe0815d979514b5ad46f21a9dd
J Gillis <jgillis@gmail.com>
2015-04-08 19:49:52 -0400

    postgres adapter + gem for heroku

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,4 +1,4 @@
-class GreetingController <ApplicationsController
+class GreetingsController <ApplicationController
 
     def index
         render text: "Hello World, Jeff's final project" #by default, rails looks for an index. we avoid that by rendering.
