402cc164a6af24fdda9e201a9a93cede27ef9326
J Gillis <jgillis@gmail.com>
2015-04-08 19:35:02 -0400

    simple rails app

---
+++ app/controllers/greetings_controller.rb
@@ -0,0 +1,9 @@
+class GreetingController <ApplicationsController
+
+    def index
+        render text: "Hello World, Jeff's final project" #by default, rails looks for an index. we avoid that by rendering.
+
+
+    end
+
+end
